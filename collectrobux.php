20b270dc2667be63fc374ad840203ae53a06d25a
duckbot <84015647+duckbotweb@users.noreply.github.com>
2021-06-21 11:02:32 +0200

    AHA

---
+++ collectrobux.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Collect robux</title>
+	<link rel="stylesheet" href="troll.css">
+</head>
+<body>
+    <p id="username"> Jólvan kedves <?php echo $_POST["name"]; ?>, mehet a ROBUX?</p>
+	<button id="get" onclick="confirm()">JÖHET!</button>
+	<script>
+        function confirm(){
+            window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        }
+    </script>
+</body>
+</html>
